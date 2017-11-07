7e55ea7e6a8c10575dcf1814db7ceeef81abcc92
4lphalfa <4lphalfa@gmail.com>
2017-11-06 21:02:36 -0800

    Added Files

---
+++ Client.java
@@ -0,0 +1,7 @@
+import javax.crypto.*;
+
+public class Client {
+    public static void main(String args[]) {
+        System.out.println("Starting Client.java...");
+    }
+}
---
+++ Server.java
@@ -0,0 +1,7 @@
+import javax.crypto.*;
+
+public class Server {
+    public static void main(String args[]) {
+        System.out.println("Starting Server.java...");
+    }
+}
