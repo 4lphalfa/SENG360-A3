88968705950b96a7eac5092ae58ae644c8fc9061
bmcewan <bryce.mcewan@yahoo.ca>
2017-11-08 19:08:53 -0800

    -Handshake for client and server mode completed and working -Prototypes for authentication methods created -Bug with mismatched security modes, will fix before next commit

--- Client.java
+++ Client.java
@@ -3,54 +3,80 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-    public static void main( String args[] ) {
-        if( args.length != 2 ){
-            System.err.println( "Usage: java Client <host name> <port number>" );
-            System.exit( 1 );
-        }
+	public static void main( String args[] ) {
+		
+		System.out.println( "Starting Client.java..." );
 
-        System.out.println( "Starting Client.java..." );
+		if( args.length != 3 ) {
+	    	System.err.println( "Usage: java Client <host name> <port number> <security option>" );
+	    	System.err.println( "Options: C for Confidentiality, I for Integrity, A for Authentication, All for all 3" );
+	    	System.exit( 1 );
+		}
 
-        String hostName = args[0];
-        int portNumber = Integer.parseInt( args[1] );
- 
-        try(
-            Socket serverSocket = new Socket( hostName, portNumber );
+		String hostName = args[0];
+		int portNumber = Integer.parseInt( args[1] );
+	 	String option = args[2].trim();
 
-            PrintWriter out = new PrintWriter( serverSocket.getOutputStream(), true );
-            BufferedReader in = new BufferedReader( new InputStreamReader( serverSocket.getInputStream() ) );
-            BufferedReader stdIn = new BufferedReader( new InputStreamReader( System.in ) );
-        ){
-            // Prepare common functions library
-            Common commonLib = new Common();
+		if( !"C".equals(option) && !"I".equals(option) && !"A".equals(option) && !"All".equals(option) ) {
+			System.err.println( "Unknown option specified" );
+			System.err.println( "Usage: java Server <port number> <security option>" );
+			System.err.println( "Options: C for Confidentiality, I for Integrity, A for Authentication, All for all");
+			System.exit( 1 );		
+		}
 
-            // Start retrieval thread
-            Thread listener = new Thread( new MessageListener( in, "Server" ) );
-            listener.start();
+		try(
+		    Socket serverSocket = new Socket( hostName, portNumber );
 
-            System.out.println("Client started!");
+		    PrintWriter out = new PrintWriter( serverSocket.getOutputStream(), true );
+		    BufferedReader in = new BufferedReader( new InputStreamReader( serverSocket.getInputStream() ) );
+		    BufferedReader stdIn = new BufferedReader( new InputStreamReader( System.in ) );
+		) {
+		 	// Prepare common functions library
+		    Common commonLib = new Common();
+		    String userInput;
+		    	
+		    // Start retrieval thread
+		    Thread listener = new Thread( new MessageListener( in, "Server" ) );
+		    listener.start();
 
-            // Send handshake message
-            // String handshakeMessage = "placeholder";
-            // commonLib.sendMessage( handshakeMessage, out, "" );
+		    System.out.println("Client started!");
 
-            // Expect handshake response
-            // userInput = stdIn.readLine();
-            
-            // TODO: Client-side authentication goes here
+			// TODO: Make this a switch statement when the other security options are impliemented
+		    if( "A".equals(option) ) { // Authentication chosen as the securtiy option
+		    	
+		    	commonLib.sendMessage( option, out, "" ); // Send the option to the server
+		    	userInput = in.readLine(); // read back the respose from the server 
+				
+				if( "Correct Mode".equals( userInput.trim() ) ) { // if the server responds that the client is in a matching mode, procede with authentication
+					System.out.println("In Correct Mode"); // TODO: remove this debugging println
+					boolean authenticated = authenticateWServer( in, out );
+
+					if( !authenticated ) { // if the authentication fails close the client
+						System.err.println("Could not authenticate with Server");
+						System.exit( 1 );
+					}
+				} else {
+					System.err.println( "Client selected security option does not match server setting, please try agian" );
+					System.exit( 1 );
+				}
+		    }
 
 
-            String userInput;
-            while( ( userInput = stdIn.readLine() ) != null ){ // TODO: fix graphical issue for when messages pop up when typing a message
-                // Send off the message
-                commonLib.sendMessage( userInput, out, "" );
-            }
-        }catch ( UnknownHostException e ){
-            System.err.println( "Don't know about host " + hostName );
-            System.exit( 1 );
-        }catch( IOException e ){
-            System.err.println( "Couldn't get I/O for the connection to " + hostName );
-            System.exit( 1 );
-        }
-    }
+	    	while( ( userInput = stdIn.readLine() ) != null ){ // TODO: fix graphical issue for when messages pop up when typing a message
+	        	// Send off the message
+	        	commonLib.sendMessage( userInput, out, "" );
+	    	}
+		} catch ( UnknownHostException e ){
+		    System.err.println( "Don't know about host " + hostName );
+		    System.exit( 1 );
+		} catch( IOException e ){
+		    System.err.println( "Couldn't get I/O for the connection to " + hostName );
+		    System.exit( 1 );
+		}
+	}
+
+	private static boolean authenticateWServer( BufferedReader in, PrintWriter out ) {
+
+		return true;
+	}
 }
--- Server.java
+++ Server.java
@@ -3,49 +3,85 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
-    public static void main( String args[] ){
-        // Set-up
-        System.out.println( "Starting Server.java..." );
 
-        if ( args.length != 1 ){
-            System.err.println( "Usage: java Server <port number>" );
-            System.exit( 1 );
-        }
-         
-        int portNumber = Integer.parseInt( args[0] );
-         
-        try(
-            ServerSocket serverSocket = new ServerSocket( Integer.parseInt( args[0] ) );
-            Socket clientSocket = serverSocket.accept();
+	public static void main( String args[] ){
+		// Set-up
+		System.out.println( "Starting Server.java..." );
 
-            PrintWriter out = new PrintWriter( clientSocket.getOutputStream(), true );
-            BufferedReader in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream() ) );
-            BufferedReader stdIn = new BufferedReader( new InputStreamReader( System.in ) );
-        ){
-            // Prepare common functions library
-            Common commonLib = new Common();
+		if ( args.length != 2 ) {
+		    System.err.println( "Usage: java Server <port number> <security option>" );
+		    System.err.println( "Option: C for Confidentiality, I for Integrity, A for Authentication, All for all options" );
+		    System.exit( 1 );
+		}
 
-            // Start retrieval thread
-            Thread listener = new Thread( new MessageListener( in, "Client" ) );
-            listener.start();
+		int portNumber = Integer.parseInt( args[0] );
+		String option = args[1].trim();
 
-            System.out.println( "Server started!" );
+		if( !"C".equals(option) && !"I".equals(option) && !"A".equals(option) && !"All".equals(option) ) {
+			System.err.println( "Unknown option specified" );
+			System.err.println( "Usage: java Server <port number> <security option>" );
+			System.err.println( "Options: C for Confidentiality, I for Integrity, A for Authentication, All for all");
+			System.exit( 1 );		
+		}
+		 
+		try(
+		    ServerSocket serverSocket = new ServerSocket( Integer.parseInt( args[0] ) );
+		    Socket clientSocket = serverSocket.accept();
 
-            String userInput;
+		    PrintWriter out = new PrintWriter( clientSocket.getOutputStream(), true );
+		    BufferedReader in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream() ) );
+		    BufferedReader stdIn = new BufferedReader( new InputStreamReader( System.in ) );
+		) {
+		    	// Prepare common functions library
+		    Common commonLib = new Common();
 
-            // Expect a handshake message
-            // userInput = stdIn.readLine();
+		    // Start retrieval thread
+		    Thread listener = new Thread( new MessageListener( in, "Client" ) );
+			listener.start();
 
-            // TODO: Server-side authentication goes here
+		    System.out.println( "Server started!" );
+
+		    String userInput;
 
 
-            while( ( userInput = stdIn.readLine() ) != null ){ // TODO: fix graphical issue for when messages pop up when typing a message
-                // Send off the message
-                commonLib.sendMessage( userInput, out, "" );
-            }
-        }catch( IOException e ){
-            System.out.println( "Exception caught when trying to listen on port " + portNumber + " or listening for a connection" );
-            System.out.println( e.getMessage() );
-        }
-    }
+
+
+			// TODO: Make this a switch statement when the other security options are impliemented
+		    if( "A".equals(option) ) { //Authentication chosen as the securtiy option
+		    	boolean authFlag = false;
+
+		    	while( !authFlag ) { //While the client hasn't authenticated loop waiting for it
+		    		userInput = in.readLine(); //expect input from client with security mode
+
+					if(userInput.trim().equals(option)) { //If client security option matches continue with authenication
+						commonLib.sendMessage( "Correct Mode", out, "" ); //Let the client know it is in correct mode
+						System.out.println("Authenicate"); // TODO: remove this debugging println
+						boolean authenticated = authenticateClient(in, out); //try and authenticate the client
+					
+						if(authenticated) { //if the client has been authenticated set bool to break loop and go to normal chat service
+							authFlag = true;							
+						} else { //if the client has not been authenticate let it know, dont break out of the loop, continue to wait for authentic client
+							System.err.println("Client Failed to Authenticate");
+							commonLib.sendMessage("Authentication Failed", out, "");
+						}
+					} else { //else client security option is incorrect, let the client know
+						commonLib.sendMessage( "Incorrect Mode", out, "");
+					}
+		    	}
+		    }
+			
+			    	
+		    while( ( userInput = stdIn.readLine() ) != null ){ // TODO: fix graphical issue for when messages pop up when typing a message
+		       	// Send off the message
+		     	commonLib.sendMessage( userInput, out, "" );
+		    }
+		} catch( IOException e ) {
+		    System.out.println( "Exception caught when trying to listen on port " + portNumber + " or listening for a connection" );
+		    System.out.println( e.getMessage() );
+		}
+	}
+
+	private static boolean authenticateClient( BufferedReader in, PrintWriter out ) {
+		return true;
+	}
 }
